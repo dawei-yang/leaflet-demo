db83ea0ce14764e3d29e83dbc48bc3f6ece4951f
Dyang <daweiyang@MacBook-Pro.home>
2022-07-26 15:33:16 -1000

    init

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <nav-bar />
+    <my-map />
+  </div>
+</template>
+
+<script>
+import MyMap from './components/MyMap.vue'
+import NavBar from './components/NavBar.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    'my-map': MyMap,
+    'nav-bar': NavBar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+      height: 100%;
+    margin: 0;
+}
+  html, body {
+    height: 100%;
+    margin: 0;
+  }
+</style>
